8350c5f5517d46fcfadc22b127dcda60eb9bf248
stabassum <sumu1993@gmail.com>
2020-01-03 11:12:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+dodger.style.background = "#FF69B4";
+
+dodger.style.left; 
+dodger.style.bottom;
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+   
+    if (right > 0) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
+
+ 
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight"){
+      moveDodgerRight();
+    }
+    else{
+        console.log("");
+    }
+  });
+
+//   document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowRight") {
+//       moveDodgerRight();
+//     }
+//   });
