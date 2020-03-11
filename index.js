55cff8002e21dce930edf1a37967853191bf5cf8
github username <github email address>
2020-03-11 14:51:31 -0400

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
    
-    if (left > 0) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
