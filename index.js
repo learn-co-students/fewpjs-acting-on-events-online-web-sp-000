08f1d27115842bbc865dce9f3407399cc6f5641a
Educhin <c.s.arinze@gmail.com>
2020-03-17 15:08:38 -0400

    This code logic allows for multidirectional movement by adding in up and down commands. The dodger can not escape on any side.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function moveDodgerRight(){
     let leftNumbers = dodger.style.left.replace('px', "");
     let left = parseInt(leftNumbers, 10);
 
-    if (left > 0) {
+    if (left < (gameBoard.clientWidth-dodger.clientWidth)) {
         dodger.style.left = `${left + 10}px`;
     }
 }
