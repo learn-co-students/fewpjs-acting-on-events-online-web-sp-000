ecde0938438f52cd64b0c3d31f7f9a123193bc92
mbade1 <mbade314@gmail.com>
2020-08-05 13:48:42 -0500

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
-let rock = document.getElementById('rock');
-rock.style.backgroundColor = "#FFFFFF";
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '');
@@ -62,3 +60,49 @@ document.addEventListener('keydown', function(e) {
     moveDodgerDown()
   }
 })
+
+
+//rock moving down 1px every 1/100th of a second
+let rock = document.getElementById("rock");
+
+function moveRockDown() {
+  var bottomNumbers = rock.style.bottom.replace('px', '');
+  var down = parseInt(bottomNumbers, 10)
+  if (down >= 0) {
+    rock.style.bottom = `${down - 1}px`
+  }
+}
+
+function moveRockUp() {
+  var bottomNumbers = rock.style.bottom.replace('px', '');
+  var up = parseInt(bottomNumbers, 10)
+  if (up >= 0) {
+    rock.style.bottom = `${up + 1}px`
+  }
+}
+
+function moveRockLeft() {
+  var leftNumbers = rock.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left >= 0) {
+    rock.style.left = `${left - 1}px`
+  }
+}
+
+function moveRockRight() {
+  var rightNumbers = rock.style.left.replace('px', '');
+  var right = parseInt(rightNumbers, 10)
+  if (right >= 0) {
+    rock.style.left = `${right + 1}px`
+  }
+}
+
+window.setInterval(function(){
+  moveRockDown();
+  moveRockLeft();
+}, 10);
+
+window.setInterval(function(){
+  moveRockUp();
+  moveRockRight();
+}, 8);
