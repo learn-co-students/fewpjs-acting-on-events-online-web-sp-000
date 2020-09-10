9cb08dab7a81e33455b26a42f5da74a21f47eafd
ksummerill <kaitlynsummerill@gmail.com>
2020-09-10 07:50:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+// prevent dodger from escaping past the left edge
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// prevent dodger from escaping past the right edge
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
