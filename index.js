3284e7ef21f27ce7529adf5324a05047a8d6c11b
Gearz <shanegeary@yahoo.com>
2020-11-29 16:47:42 -0700

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function moveDodgerLeft() {
     }
   }
 
-  function moveDodgerRight() {
+function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
 
@@ -19,7 +19,7 @@ function moveDodgerLeft() {
     }
   } 
 
-  document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
     }
