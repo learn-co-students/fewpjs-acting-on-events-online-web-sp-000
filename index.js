fe62d95134bc78144b7241fdae8d86d406dccb64
Jenise Ruehle <jeniseruehle@gmail.com>
2021-07-03 13:08:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ function moveDodgerLeft() {
     }
 }
 
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+});
+
 function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
@@ -17,3 +23,9 @@ function moveDodgerRight() {
         dodger.style.left = `${left + 1}px`;
     }
 }
+  
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+});
